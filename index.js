388b89d4c730f06e863d1eebc8e627e32d21109d
Jonas Nascimento <jonashnascimento@gmail.com>
2024-06-28 11:17:32 -0300

    database connection and Create and Read queries

--- index.js
+++ index.js
@@ -13,27 +13,55 @@ app.set("view engine", "handlebars")
 
 app.use(express.static("public"))
 
+app.use(
+    express.urlencoded({
+        extended:true
+    })
+)
+
+app.use(express.json())
+
 const port = 3000
 
 app.get("/", (req, res) => {
-    const tasks = [
-        {
-            description: "Add timer to the system",
-            pomodoros : 1
-        },
-        {
-            description: "Implement the persistence",
-            pomodoros : 2
-        },
-        {
-            description: "Style the home page",
-            pomodoros : 2
-        },
-    ]
+    const sql = "SELECT * FROM tasks"
+    conn.query(sql, (err, data) => {
+        if(err) {
+            console.log(err)
+            return;
+        }
+        const tasks = data ?? []
 
-    res.render("home", {tasks})
+        res.render("home", {tasks})
+    }) 
 })
 
-app.listen(port, () => {
-    console.log("Application started on port:",port)
+app.post("/tasks", (req,res) => {
+    const description = req.body.description
+    const pomodoros = req.body.pomodoros
+
+    const query = `INSERT INTO tasks (description, pomodoros) VALUES ('${description}', '${pomodoros}')` 
+
+    conn.query(query, (err) => {
+        if(err) console.log(err)
+
+        res.redirect("/")
+    })
+})
+
+const conn = mysql.createConnection({
+    host:"localhost",
+    user: "root",
+    password: "",
+    database: "test"
+})
+
+conn.connect((err) => {
+    if(err) console.log(err)
+
+    console.log("Connected to MySQL Database")
+
+    app.listen(port, () => {
+        console.log("Application started on port:",port)
+    })
 })
