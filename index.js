ccc169da78c153357f2441561bbf70205c8bebf8
Jonas Nascimento <jonashnascimento@gmail.com>
2024-07-02 15:06:35 -0300

    update and delete services

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 const exphbs = require("express-handlebars")
-const mysql = require("mysql")
+const pool = require("./db/conn")
 
 const hbs = exphbs.create({
     partialsDir: ["views/partials"]
@@ -23,12 +23,12 @@ app.use(express.json())
 
 const port = 3000
 
+//read
 app.get("/", (req, res) => {
     const sql = "SELECT * FROM tasks"
-    conn.query(sql, (err, data) => {
+    pool.query(sql, (err, data) => {
         if(err) {
             console.log(err)
-            return;
         }
         const tasks = data ?? []
 
@@ -36,32 +36,89 @@ app.get("/", (req, res) => {
     }) 
 })
 
+//read by id
+app.get("/tasks", (req, res) => {
+    const id = req.query.id
+    if(!id) {
+        res.redirect("/")
+        return
+    }
+
+    const query = `SELECT * FROM tasks WHERE ?? = ?`
+    const data = ["id", id]
+    pool.query(query, data, (err, data) => {
+        if(err) {
+            console.log(err)
+        }
+
+        res.render('home', {tasks:data??[]})
+    })
+})
+
+//create
 app.post("/tasks", (req,res) => {
     const description = req.body.description
     const pomodoros = req.body.pomodoros
 
-    const query = `INSERT INTO tasks (description, pomodoros) VALUES ('${description}', '${pomodoros}')` 
+    const query = `INSERT INTO tasks (??, ??) VALUES (?, ?)` 
+    const data  = ["description", "pomodoros", description, pomodoros]
 
-    conn.query(query, (err) => {
+    pool.query(query, data, (err) => {
         if(err) console.log(err)
 
         res.redirect("/")
     })
 })
 
-const conn = mysql.createConnection({
-    host:"localhost",
-    user: "root",
-    password: "",
-    database: "test"
+//edit view
+app.get("/tasks/edit/:id", (req, res) => {
+    const id = req.params.id
+    
+    const sql = `SELECT * FROM tasks WHERE ?? = ?`
+    const data = ["id", id]
+
+    pool.query(sql, data, (err, data) => {
+        if(err){
+            console.log(err)
+            return
+        }
+        
+        res.render("edit_task", {task: data[0]})
+    })
 })
 
-conn.connect((err) => {
-    if(err) console.log(err)
+//update
+app.post("/tasks/updatetask", (req, res) => {
+    const {id, description, pomodoros} = req.body
 
-    console.log("Connected to MySQL Database")
+    const sql = `UPDATE tasks SET ?? = ?, ?? = ? WHERE ?? = ?`
+    const data  = ["description", description, "pomodoros",pomodoros,  "id", id]
 
-    app.listen(port, () => {
-        console.log("Application started on port:",port)
+    pool.query(sql, data, (err) => {
+        if(err) {
+            console.log(err)
+            return;
+        }
+        res.redirect("/")
     })
+})
+
+//delete
+app.post("/tasks/delete/:id", (req, res) => {
+    const id = req.params.id
+
+    const sql = `DELETE FROM tasks WHERE ??=?`
+    const data = ["id", id]
+
+    pool.query(sql, data, (err) => {
+        if(err) {
+            console.log(err)
+            return
+        }
+        res.redirect("/")
+    })
+})
+
+app.listen(port, () => {
+    console.log("Application started on port:",port)
 })
