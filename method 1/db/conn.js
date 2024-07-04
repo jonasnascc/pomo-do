7144e0a915861f99b55fd39250ee2d595c8f85b6
Jonas Nascimento <jonashnascimento@gmail.com>
2024-07-04 18:05:18 -0300

    started mvc method

--- method 1/db/conn.js
+++ method 1/db/conn.js
@@ -1,6 +1,6 @@
 const {Sequelize} = require('sequelize')
 
-const sequelize = new Sequelize("pomododb","root", "", {
+const sequelize = new Sequelize("pomododb", "root", "", {
     host: "localhost",
     dialect: "mysql"
 })
