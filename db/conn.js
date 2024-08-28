7ff9f1ea76e97a9ff760917923575424ceedd386
Jonas Nascimento <jonashnascimento@gmail.com>
2024-08-28 11:28:29 -0300

    changed dialect module

--- db/conn.js
+++ db/conn.js
@@ -1,10 +1,11 @@
+const pg = require('pg')
 const {Sequelize} = require('sequelize')
 
-const DATABASE_URL = process.env?.DATABASE_URL ?? "" 
+const DATABASE_URL = process.env?.DATABASE_URL ?? "postgresql://pomododb_owner:kfzJGDEmn0W5@ep-dark-breeze-a5bt0lt2.us-east-2.aws.neon.tech/pomododb?sslmode=require" 
 
 console.log()
 
-const sequelize = new Sequelize(DATABASE_URL, {dialect: "postgres"})
+const sequelize = new Sequelize(DATABASE_URL, {dialect: "postgres", dialectModule: pg})
 
 try{
     sequelize.authenticate()
